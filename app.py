fe11302052bf7a70c27cfda8bb7ccdf6ce30b0cf
Jeff <jeffreywjlim@gmail.com>
2025-02-01 20:57:06 -0500

    50

--- app.py
+++ app.py
@@ -1,39 +1,17 @@
+import os
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-import os
 
+# Get database URL from Railway
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-def retrieve_user(email):
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    
-    user_email = email
-    select_sql = """
-    SELECT id, email FROM "user" WHERE email = %s;
-    """
-    cur.execute(select_sql, (user_email,))
-    user_info = cur.fetchone()
-    try:
-        if user_info:
-            return {
-                "id": user_info[0],
-                "email": user_info[1]
-            }
-        else:
-            return None
-    except:
-        pass
-    finally:
-        cur.close()
-        conn.close()
-    
-    
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello from Flask on Railway!"})
 
 @app.route('/get_user', methods=['GET'])
 def get_user():
@@ -41,10 +19,15 @@ def get_user():
     if not email:
         return jsonify({'error': 'Email parameter is required'}), 400
 
-    user_data = retrieve_user(email)
-    
-    if user_data:
-        return jsonify(user_data), 200
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute('SELECT id, email FROM "user" WHERE email = %s;', (email,))
+    user_info = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    if user_info:
+        return jsonify({"id": user_info[0], "email": user_info[1]}), 200
     else:
         return jsonify({'error': 'User not found'}), 404
 
