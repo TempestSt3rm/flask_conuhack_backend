d75c36695c4eabef9220160d6a54b51bb7a0ab7c
Jeff <jeffreywjlim@gmail.com>
2025-02-06 22:54:59 -0500

    test change

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 def home():
     return jsonify({"message": "Hello from Flask on Railway!"})
 
+#This is the test change 
 
 if __name__ == '__main__':
     app.run(debug=True)
