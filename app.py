a7bb70b418981f1735ca5fdfb4e07d27bbb8ca54
Jeff <jeffreywjlim@gmail.com>
2025-02-06 22:52:36 -0500

    new chages

--- app.py
+++ app.py
@@ -13,23 +13,6 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 def home():
     return jsonify({"message": "Hello from Flask on Railway!"})
 
-@app.route('/get_user', methods=['GET'])
-def get_user():
-    email = request.args.get('email')
-    if not email:
-        return jsonify({'error': 'Email parameter is required'}), 400
-
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    cur.execute('SELECT id, email FROM "user" WHERE email = %s;', (email,))
-    user_info = cur.fetchone()
-    cur.close()
-    conn.close()
-
-    if user_info:
-        return jsonify({"id": user_info[0], "email": user_info[1]}), 200
-    else:
-        return jsonify({'error': 'User not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
